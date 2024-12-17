82ec67f37947e4c2dd421e6648967d2fc2b572cd
Khrystyna Peryk <kristik991@gmail.com>
2024-12-17 15:53:48 +0400

    before server actions implem. (delete actions folder) - LOGIN

---
+++ schemas/index.ts
@@ -0,0 +1,11 @@
+import * as z from 'zod'
+
+// validation on login
+export const LoginSchema = z.object({
+    email: z.string().email({
+        message: "Email is required"
+    }),
+    password: z.string().min(1, {
+        message: "Password is required"
+    })
+})
